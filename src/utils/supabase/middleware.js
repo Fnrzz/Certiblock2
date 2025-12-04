cb9b693e9dfa5df7c6528f03ce1463f17402cbb6
Fnrzz <faridnurrganteng123@gmail.com>
2025-12-04 19:29:41 +0700

    [PUSH] fixing cspHeader fixing

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -8,13 +8,12 @@ export async function updateSession(request) {
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${
+    script-src 'self' 'nonce-${nonce}' ${
     isDev ? "'unsafe-eval'" : ""
   } https://hcaptcha.com https://*.hcaptcha.com;
     style-src 'self' ${
       isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`
     } https://hcaptcha.com https://*.hcaptcha.com;
-    style-src-attr 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
