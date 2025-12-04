0683cd80a0094e9acfcefc455007aac81cef4540
Fnrzz <faridnurrganteng123@gmail.com>
2025-12-04 20:30:23 +0700

    [PUSH] fixing cspHeader

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -5,9 +5,13 @@ export async function updateSession(request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
+  const isDev = process.env.NODE_ENV === "development";
+
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://hcaptcha.com https://*.hcaptcha.com https://vercel.live;
+    script-src 'self' 'nonce-${nonce}' ${
+    isDev ? "'unsafe-eval'" : ""
+  } https://hcaptcha.com https://*.hcaptcha.com https://vercel.live;
     style-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com;
     img-src 'self' blob: data:;
     font-src 'self';
