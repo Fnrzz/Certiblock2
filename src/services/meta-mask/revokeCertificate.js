c59f6d711b91e9fa381e5e8ca36bf529d7105344
Fnrzz <faridnurrganteng123@gmail.com>
2025-12-15 13:04:34 +0700

    [PUSH] fixing revokeCertificate

--- src/services/meta-mask/revokeCertificate.js
+++ src/services/meta-mask/revokeCertificate.js
@@ -1,20 +1,21 @@
 import { ethers } from "ethers";
 import contractABI from "@/contracts/certificateRegistryABI.json";
 export const revokeCertificate = async (hash) => {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(
+    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    contractABI,
+    signer
+  );
   try {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-    const contract = new ethers.Contract(
-      process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-      contractABI,
-      signer
-    );
     const tx = await contract.revokeCertificate(hash);
     return tx.hash;
   } catch (error) {
-    if (error.message.includes("CertificateNotFound")) {
+    const decodedError = contract.interface.parseError(error.data);
+    if (decodedError.name === "CertificateNotFound") {
       throw new Error("Certificate not found");
-    } else if (error.message.includes("NotOwner")) {
+    } else if (decodedError.name === "NotOwner") {
       throw new Error("You are not the owner");
     } else if (error.code === "ACTION_REJECTED") {
       throw new Error("Transaction was rejected by the user in MetaMask.");
